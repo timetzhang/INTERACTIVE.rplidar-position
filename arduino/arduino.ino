aebc6f87414c67ede6cf64a6185caec84cfe9226
timetzhang <367271@qq.com>
2018-11-09 15:47:25 +0800

    fix

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,24 +1,27 @@
-
 #include <Keyboard.h>
 #include <RPLidar.h>
 
-#define BUTTON 12 //
-// You need to create an driver instance
+#define ENABLE 13 //Grounded this pin to enable the RPLIDAR
+#define RPLIDAR_MOTOR 3 // this PWM pin for control the speed of RPLIDAR's motor.
+
 RPLidar lidar;
-uint8_t keys[] = {'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'};
+
+uint8_t keys[9] = {'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'};
 long lastKeyTime;
 
+//确定x为负还是正
 int operation;
-#define RPLIDAR_MOTOR 3 // The PWM pin   for   ontrol the speed of RPLIDAR's motor.
-// This pin should connected with the RPLIDAR's MOTOCTRL signal
 
+//探测画幅, 单位为 0.1mm, 1000为1米
+int left_width = 2000;
+int right_width = 2000;
+int height = 2000;
 
 void setup() {
-  // bind the RPLIDAR driver to the arduino hardware serial
   Serial.begin(115200);
   Serial1.begin(115200);
 
-  pinMode(BUTTON, INPUT);
+  pinMode(ENABLE, INPUT_PULLUP);
 
   Keyboard.begin();
   lidar.begin(Serial1);
@@ -28,54 +31,36 @@ void setup() {
 }
 
 void loop() {
-  if (digitalRead(BUTTON) == HIGH) {
+  if (digitalRead(ENABLE) == LOW) {
     if (IS_OK(lidar.waitPoint())) {
       float distance = lidar.getCurrentPoint().distance; //distance value in mm unit
       float angle    = lidar.getCurrentPoint().angle; //anglue value in degree
       bool  startBit = lidar.getCurrentPoint().startBit; //whether this point is belong to a new scan
       byte  quality  = lidar.getCurrentPoint().quality; //quality of the current measurement
 
-      //右边1/4半球度数处理
-      if (angle >= 270) {
-        angle = 360 - angle;
-        operation = 1;
-      }
-      else {
-        operation = -1; //左边为-x
-      }
-
-      int x = operation * sin(angle * (PI / 180)) * distance;
-      int y = cos(angle * (PI / 180)) * distance;
-
-      if (x != 0 && y != 0 && x > -1700 && x < 1700 && y < 1300 && ((angle >= 0 && angle <= 90) || (angle >= 270 && angle <= 360))) {
-        //      Serial.print("distance:");
-        //      Serial.print(distance);
-        //      Serial.print("/");
-
-
-        //将角度转成弧度 angle * (PI / 180)
-        //      Serial.print("angle:");
-        //      Serial.print(angle);
-        //      Serial.print("/");
-        //      Serial.print("sin(angle):");
-        //      Serial.print(sin(angle * (PI / 180)));
-        //      Serial.print("/");
-        //      Serial.print("cos(angle):");
-        //      Serial.print(cos(angle * (PI / 180)));
-        //      Serial.print("/");
-
-        if (millis() > lastKeyTime + 100) {
-          for (int i = 0; i < 8; i++) {
-            Keyboard.release(keys[i]);
-          }
-          Keyboard.press(keys[int(map(x, -1000, 1000, 0, 9))]);
-          lastKeyTime = millis();
+      //如果检测到距离，开始工作
+      if (distance > 0) { 
+        // 左边扇形与右边扇形度数处理
+        if (angle >= 270) {
+          angle = 360 - angle;
+          operation = 1; //左边为 +x
+        }
+        else {
+          operation = -1; //左边为 -x
         }
 
-        Serial.print(x);
-        Serial.print(",");
-        Serial.println(y);
-        delay(50);
+        //通过sin和cos得出向量(x,y)
+        int x = operation * sin(angle * (PI / 180)) * distance;
+        int y = cos(angle * (PI / 180)) * distance;
+
+        //RPLIDAR CABLE往上, 所以处理 0 - 90 与 270 - 360
+        if (x > -left_width && x < right_width && y < height && ((angle >= 0 && angle <= 90) || (angle >= 270 && angle <= 360))) {
+          Serial.print(x);
+          Serial.print(",");
+          Serial.println(y);
+          Keyboard.write(keys[int(map(x, -left_width, right_width, 0, sizeof(keys)))]);
+          delay(50);
+        }
       }
     } else {
       analogWrite(RPLIDAR_MOTOR, 0); //stop the rplidar motor
@@ -85,18 +70,14 @@ void loop() {
       if (IS_OK(lidar.getDeviceInfo(info, 100))) {
         // detected...
         lidar.startScan();
-
         // start motor rotating at max allowed speed
         analogWrite(RPLIDAR_MOTOR, 190);
         delay(1000);
       }
     }
   }
-  if (digitalRead(BUTTON) == LOW) {
-    for (int i = 0; i < 8; i++) {
-      Keyboard.release(keys[i]);
-    }
+  if (digitalRead(ENABLE) == HIGH) {
     analogWrite(RPLIDAR_MOTOR, 0);
-    delay(1000);
+    delay(2000);
   }
 }
